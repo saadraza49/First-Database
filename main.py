9289c9aed505c34b86b52ab57c48e24b9ea75a57
Muhammad Saad Raza <msaadraza49@gmail.com>
2026-04-03 23:31:31 +0500

    made the products table and injected dependancy

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import User
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
--- models.py
+++ models.py
@@ -10,3 +10,9 @@ class User(Base):
     age = Column(Integer , index=True)
     email = Column(String ,unique=True , index=True)
     
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer , primary_key= True , index=True)
+    name = Column(String , index=True)
+    price = Column(Integer , index=True)
+    description = Column(String , index=True)
